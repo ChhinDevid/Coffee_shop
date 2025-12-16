0f24de91c2239655efd7fd7392aac53d733c2cda
Chhin Devid <chhindevid@gmail.com>
2025-12-16 13:30:45 +0700

    Coffee shop

---
+++ src/main/java/com/setec/services/MyTelegramBot.java
@@ -0,0 +1,30 @@
+package com.setec.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+
+import lombok.Data;
+
+@Service
+@Data
+public class MyTelegramBot {
+
+    @Value("${token}")
+    private String token;
+
+    @Value("${chat_id}")
+    private long chat_id;
+
+    private TelegramBot bot;
+
+    public SendResponse sendMessage(String text) {
+        if (bot == null)
+            bot = new TelegramBot(token);
+
+        return bot.execute(new SendMessage(chat_id, text));
+    }
+}
